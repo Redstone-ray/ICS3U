999ff0fd86e93b3d49d44154a3d3e505d34f3c39
Redstone_ray <21973258+Redstone-ray@users.noreply.github.com>
2019-11-13 17:57:55 -0500

    Fixed a small error, added goals for the game

--- Introduction/src/flowcontrol/lotterynew.java
+++ Introduction/src/flowcontrol/lotterynew.java
@@ -4,13 +4,10 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.ArrayList;
 import java.util.List;
-
-
-
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Snippet {
 	@SuppressWarnings("deprecation")
@@ -18,15 +15,14 @@ public class Snippet {
 		DataInputStream input = new DataInputStream(System.in);
 		String choice;
 		double bankBalance, intRate, endingBal;
-		int number, age;
+		int number, age = 20;
 		NumberFormat d = new DecimalFormat("#0.00");
 		NumberFormat e = new DecimalFormat("#0.00");
 		Random rand = new Random();
 		Random startingRate = new Random();
-		
-		
-		//Starting balance
-		List<Integer> list = new ArrayList<Integer>();
+
+		// Starting balance
+		List<Integer> list = new ArrayList<>();
 		list.add(200);
 		list.add(200);
 		list.add(200);
@@ -40,30 +36,27 @@ public class Snippet {
 		list.add(2000);
 		list.add(5000);
 		list.add(10000);
-		//randomly select an amount from the array list
+		// randomly select an amount from the array list
 		Snippet obj = new Snippet();
-		
-		//apply the bank balance
-		bankBalance=obj.getRandomList(list);
-		//prompte the use to enter starting balance
-		System.out.println("Your started at the age of "+age+", with a balance of: $" +bankBalance);
-		//randomly generate the intRate
+
+		// apply the bank balance
+		bankBalance = obj.getRandomList(list);
+		// prompte the use to enter starting balance
+		System.out.println("Your started at the age of " + age + ", with a balance of: $" + bankBalance);
+		// randomly generate the intRate
 		intRate = 1.5 + (5.5 - 2.5) * startingRate.nextDouble();
-		System.out.print("Your rate of interest is "+e.format(intRate)+ "%, good luck!");
-		endingBal = bankBalance + (bankBalance * (intRate / 100));
+		System.out.println("Your rate of interest is " + e.format(intRate) + "%, good luck!");
+		endingBal = bankBalance + bankBalance * (intRate / 100);
 		for (int i = 20; i <= 80; i++) {
 
-			System.out.println("The balance at age " + i + " years is " + d.format(endingBal));
-			endingBal = endingBal + (endingBal * (intRate / 100));
-			
-			
-			
-			
-			//stock system
+			System.out.println("The balance at age " + i + " is " + d.format(endingBal));
+			endingBal = endingBal + endingBal * (intRate / 100);
+
+			// stock system
 			//
 			//
-			System.out.println(
-					"Would you like to play lottery for 200 dollors? Winning will double your interest rate y/n");
+			System.out.print(
+					"Would you like to play lottery for 200 dollors?\n Winning will double your interest rate y/n");
 			choice = input.readLine();
 			if (choice.equalsIgnoreCase("Y") && endingBal >= 200) {
 				endingBal -= 200;
@@ -72,7 +65,7 @@ public class Snippet {
 				number = Integer.parseInt(input.readLine());
 				if (number == n) {
 					intRate += intRate;
-					System.out.println("The number is correct! Your Interst Rate is now " + intRate);
+					System.out.println("The number is correct! Your Interst Rate is now " + e.format(intRate));
 				} else {
 					System.out.println("Sorry, you lose the lottery!");
 				}
@@ -83,41 +76,49 @@ public class Snippet {
 				System.out.println("Okay, proceed to year " + (i + 1));
 
 			}
-			//end of stock system
+			// end of stock system
 			//
-			//			
-		
-			
-			if (endingBal <= 50) {
-				System.out.println("Slave");
-			} else if (endingBal <= 500) {
-				System.out.println("Poor");
-			} else if (endingBal <= 1000) {
-				System.out.println("Communist");
-			} else if (endingBal <= 2000) {
-				System.out.println("Medium Class");
-			} else if (endingBal <= 5000) {
-				System.out.println("Capitalist");
-			} else if (endingBal <= 10000) {
-				System.out.println("Dictorship");
-			} else if (endingBal <= 20000) {
-				System.out.println("Java Programmer");
-			} else if (endingBal <= 100000) {
-				System.out.println("Overlord");
-			} else if (endingBal <= 200000) {
-				System.out.println("Bank Rubber");
-			} else if (endingBal <= 500000) {
-				System.out.println("Donald Trump");
-			}
+			//
 
+			System.out.println("-----------------------------------------");
+			System.out.println("You have reached the level:");
+			if (endingBal <= 50) {
+				System.out.println("1.Slave");
+			} else if (endingBal <= 500) {
+				System.out.println("2.Poor");
+			} else if (endingBal <= 1000) {
+				System.out.println("3.Communist");
+			} else if (endingBal <= 2000) {
+				System.out.println("4.Medium Class");
+			} else if (endingBal <= 5000) {
+				System.out.println("5.Capitalist");
+			} else if (endingBal <= 10000) {
+				System.out.println("6.Dictorship");
+			} else if (endingBal <= 20000) {
+				System.out.println("7.Java Programmer");
+			} else if (endingBal <= 100000) {
+				System.out.println("8.Html coder");
+			} else if (endingBal <= 200000) {
+				System.out.println("9.Bank Rubber");
+			} else if (endingBal <= 800000) {
+				System.out.println("10.Stock master");
+			} else if (endingBal <= 2000000) {
+				System.out.println("11.Godlike");
+			} else if (endingBal <= 6000000) {
+				System.out.println("11.Tax collector");
+			} else if (endingBal <= 10000000) {
+				System.out.println("Donald Trump");
+				System.out.println("You win the game!");
 			}
-		
+			System.out.println("-----------------------------------------");
 		}
+
+	}
+
 	public int getRandomList(List<Integer> list) {
 
-	    //0-5
-	    int index = ThreadLocalRandom.current().nextInt(list.size());		
-	    return list.get(index);
+		// 0-5
+		int index = ThreadLocalRandom.current().nextInt(list.size());
+		return list.get(index);
 	}
-	}
-
+}
