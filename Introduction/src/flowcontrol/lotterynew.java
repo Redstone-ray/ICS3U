df32f6bf1f67bd65f38471168b42a8fb2ab6652f
Xan1924 <57727554+Xan1924@users.noreply.github.com>
2019-11-14 23:20:48 -0500

    Update lotterynew.java

--- Introduction/src/flowcontrol/lotterynew.java
+++ Introduction/src/flowcontrol/lotterynew.java
@@ -26,6 +26,8 @@ public class Snippet {
 		Random rand = new Random();
 		Random startingRate = new Random();
 		Random stockUse = new Random();
+		String yesorno = "";
+		int waiter= 0;
 
 		// Starting balance
 		List<Integer> list = new ArrayList<>();
@@ -60,27 +62,27 @@ public class Snippet {
 		//applys the age
 		int age = startAgeObject.getRandomAge(ageAtStart);
 		// prompt the use to enter starting balance
-		out.println("You started at the age of " + age + ", with a balance of: $" + w.format(bankBalance));
+		System.out.println("You started at the age of " + age + ", with a balance of: $" + w.format(bankBalance));
 
 		// randomly generate the intRate
 		intRate = 2.5 + (8.5 - 2.5) * startingRate.nextDouble();
-		out.println("Your rate of interest is " + e.format(intRate) + "%, good luck!");
+		System.out.println("Your rate of interest is " + e.format(intRate) + "%, good luck!");
 
 		//Trade age for extra starting interest rate
-		out.println("Would you like to trade some years of your life for "+d.format(intRate)+ "% plus higher percent of interest rate? (To a maximum of 5 years)");
-		out.print("A number between 0-5 indicates the age you want to trade off:");
+		System.out.println("Would you like to trade some years of your life for "+d.format(intRate)+ "% plus higher percent of interest rate? (To a maximum of 5 years)");
+		System.out.print("A number between 0-5 indicates the age you want to trade off:");
 		ageToTrade=Integer.parseInt(input.readLine());
 		if (ageToTrade>=1 && ageToTrade<=5) {
 			intRate+=ageToTrade;
 			age+=ageToTrade;
 			//show a message of new age if the user input a ageToTrade>0
-			out.println("Success, your new interest is now: "+d.format(intRate)+"%");
-			out.println(ageToTrade+" years had been added to your age, you are now "+age);
+			System.out.println("Success, your new interest is now: "+d.format(intRate)+"%");
+			System.out.println(ageToTrade+" years had been added to your age, you are now "+age);
 		}
 		else if (ageToTrade==0) {
-			out.println("Ok, continue without trading age");
+			System.out.println("Ok, continue without trading age");
 		}
-		out.println("----------------------------------------");
+		System.out.println("----------------------------------------");
 		endingBal = bankBalance + bankBalance * (intRate / 100);
 
 		//set a value for trumpstock, stock 1
@@ -88,31 +90,31 @@ public class Snippet {
 
 		while (age<= 80) {
 
-			out.println("The balance at age " + age + " is $" + d.format(endingBal));
+			System.out.println("The balance at age " + age + " is $" + d.format(endingBal));
 			endingBal = endingBal + endingBal * (intRate / 100);
 
 			// lottery system
 			//
 			//
-			out.print(
-					"Would you like to play lottery for $200?\n Winning will double your interest rate y/n");
+			System.out.print(
+					"Would you like to play lottery for $200?\nWinning will double your interest rate y/n");
 			choice = input.readLine();
 			if (choice.equalsIgnoreCase("Y") && endingBal >= 200) {
 				endingBal -= 200;
 				double n = rand.nextInt(5);
-				out.println("Please enter a Natural Number between 0-5");
+				System.out.println("Please enter a Natural Number between 0-5");
 				number = Integer.parseInt(input.readLine());
 				if (number == n) {
 					intRate += intRate;
-					out.println("The number is correct! Your Interest Rate is now %" + e.format(intRate));
+					System.out.println("The number is correct! Your Interest Rate is now %" + e.format(intRate));
 				} else {
-					out.println("Sorry, you lose the lottery!");
+					System.out.println("Sorry, you lose the lottery!");
 				}
 			} else if (endingBal <= 50) {
-				out.println("You don't have enough money! Poor you!");
+				System.out.println("You don't have enough money! Poor you!");
 
 			} else {
-				out.println("OK, no lottery");
+				System.out.println("OK, no lottery");
 
 			}
 			// end of lottery system
@@ -121,8 +123,8 @@ public class Snippet {
 
 
 			//Start of stock system
-			out.println("You have "+trumpStockNumber+" trumpStock on your account;");
-			out.println("Would you like to purchase stocks? Y/N");
+			System.out.println("You have "+trumpStockNumber+" trumpStock(s) on your account");
+			System.out.println("Would you like to purchase stocks? Y/N");
 			choiceStock=input.readLine();
 			double stockIncreaseDecrease = rand.nextInt(8);
 			double stockChangeAmount=0.0 + (0.10 - 0.0) * stockUse.nextDouble();
@@ -143,10 +145,10 @@ public class Snippet {
 			}
 			String plusOrMinus="";
 			if(choiceStock.equalsIgnoreCase("y")) {
-				out.println("*******************");
-				out.println("Here are the current stock(s) open today");
-				out.println();
-				out.println("Stock name\tprice\tchange");
+				System.out.println("*******************");
+				System.out.println("Here are the current stock(s) open today");
+				System.out.println();
+				System.out.println("Stock name\t\tPrice\tChange");
 				//Show percent increase/decrease
 				if (stockIncreaseDecrease<=3) {
 					plusOrMinus="+"+d.format(stockChangeAmount*100)+"%";
@@ -163,110 +165,132 @@ public class Snippet {
 					plusOrMinus="-10%";
 				}
 
-				out.println("1.TrumpStock\t"+d.format(trumpStock)+"\t"+plusOrMinus);
-				out.println("*******************");
-				out.println("Press 'b' for buy, 's' for sell, enter any other key to proceed to next year");
+				System.out.println("1.TrumpStock\t$"+d.format(trumpStock)+"\t"+plusOrMinus);
+				System.out.println("*******************");
+				System.out.println("Press 'b' for buy, 's' for sell, enter any other key to proceed to next year");
 				int buyOrSellAmount;
 				String buyOrSell=input.readLine();
 				//buy, add current amount of stock
 				if (buyOrSell.equalsIgnoreCase("b")) {
-					out.print("Input buy amount(number of stocks):");
+					System.out.print("Input buy amount(number of stocks):");
 					buyOrSellAmount=Integer.parseInt(input.readLine());
 					//check for enough money
 					if (buyOrSellAmount>0 && endingBal>=trumpStock*buyOrSellAmount) {
 						endingBal=endingBal-trumpStock*buyOrSellAmount;
 						trumpStockNumber+=buyOrSellAmount;
-						out.println("Success, you purchased "+buyOrSellAmount+" stocks");
-						out.println("You now have "+trumpStockNumber+ "stocks");
+						System.out.println("Success, you purchased "+buyOrSellAmount+" stocks");
+						System.out.println("You now have "+trumpStockNumber+ " stocks");
 					}
 					else {
-						out.println("Invaild number, you might not have enough money");
+						System.out.println("Invaild number, you might not have enough money");
 					}
 				}
 				else if (buyOrSell.equalsIgnoreCase("s")) {
-					out.print("Input sell amount:");
+					System.out.print("Input sell amount:");
 					buyOrSellAmount=Integer.parseInt(input.readLine());
 					if (trumpStockNumber>=buyOrSellAmount){
 						endingBal=endingBal+trumpStock*buyOrSellAmount;
 						trumpStockNumber-=buyOrSellAmount;
-						out.println("Success, you sold "+buyOrSellAmount+" stocks");
-						out.println("You now have "+trumpStockNumber+ "stocks");
+						System.out.println("Success, you sold "+buyOrSellAmount+" stocks");
+						System.out.println("You now have "+trumpStockNumber+ " stocks");
 					}
 					else {
-						out.println("Sorry, that is an invaild number, please enter a number within the amount you have: "+trumpStockNumber);
+						System.out.println("Sorry, that is an invaild number, please enter a number within the amount you have: "+trumpStockNumber);
 					}
 				}
 				else {
-					out.println("OK, proceeding to next year");
+					System.out.println("OK, proceeding to next year");
 				}
 
 			}
-			out.println("-----------------------------------------");
-			out.println("You have reached the level:");
+			System.out.println("-----------------------------------------");
+			System.out.println("You have reached the level:");
 
 			String lvl="";
 			int lvlmon=0;
 
 
 			if (endingBal <= 50) {
-				out.println("1.Slave");
+				System.out.println("Lvl 1: Slave");
 				lvl = "Poor";
 				lvlmon = 500;
 			} else if (endingBal <= 500) {
-				out.println("2.Poor");
+				System.out.println("Lvl 2: Poor");
 				lvl = "Communist";
 				lvlmon = 1000;
 			} else if (endingBal <= 1000) {
-				out.println("3.Communist");
+				System.out.println("Lvl 3: Communist");
 				lvl = "Medium Class";
 				lvlmon = 2000;
 			} else if (endingBal <= 2000) {
-				out.println("4.Medium Class");
+				System.out.println("Lvl 4: Medium Class");
 				lvl = "Capitalist";
 				lvlmon = 5000;
 			} else if (endingBal <= 5000) {
-				out.println("5.Capitalist");
+				System.out.println("Lvl 5: Capitalist");
 				lvl = "Dictatorship";
 				lvlmon = 10000;
 			} else if (endingBal <= 10000) {
-				out.println("6.Dictatorship");
+				System.out.println("Lvl 6: Dictatorship");
 				lvl = "Java Programmer";
 				lvlmon = 20000;
 			} else if (endingBal <= 20000) {
-				out.println("7.Java Programmer");
+				System.out.println("Lvl 7: Java Programmer");
 				lvl = "Html coder";
 				lvlmon = 100000;
 			} else if (endingBal <= 100000) {
-				out.println("8.Html coder");
+				System.out.println("Lvl 8: Html Coder");
 				lvl = "Bank Rubber";
 				lvlmon = 200000;
 			} else if (endingBal <= 200000) {
-				out.println("9.Bank Rubber");
+				System.out.println("Lvl 9: Bank Rubber");
 				lvl = "Bank Rubber";
 				lvlmon = 800000;
 			} else if (endingBal <= 800000) {
-				out.println("10.Stock master");
+				System.out.println("Lvl 10: Stock Master");
 				lvl = "Godlike";
 				lvlmon = 2000000;
 			} else if (endingBal <= 2000000) {
-				out.println("11.Godlike");
+				System.out.println("Lvl 11: Godlike");
 				lvl = "Tax collector";
 				lvlmon = 6000000;
 			} else if (endingBal <= 6000000) {
-				out.println("11.Tax collector");
+				System.out.println("Lvl 12: Tax Collector");
 				lvl = "Donald Trump";
 				lvlmon = 10000000;
 
 			}
-			else if (endingBal <= 10000000) {
-				out.println("Donald Trump");
-				out.println("You win the game!");
+			else if (endingBal >= 6000000 && waiter == 0) {
+				System.out.println("Lvl MAX: Donald Trump");
+				System.out.println("You win the game!");
+				System.out.println("Continue? Y/N");
+				while (waiter == 0) {
+					yesorno = input.readLine();
+					if (yesorno.equalsIgnoreCase("n")) {
+						System.exit(0);
+					} else if
+					(yesorno.equalsIgnoreCase("y")) {
+						System.out.println("OK");
+						waiter = 1;
+						lvl = "Donald Trump";
+					} else {
+						System.out.println("Use 'y' for Yes and 'n' for no");
+				}	
 			}
 
-			if (endingBal < 10000000) {
-				out.println("You need $" + d.format(lvlmon - endingBal) + " to reach the next level: " + lvl);
+		}
+			else {
+				System.out.println("Lvl MAX: Donald Trump");
+				lvl = "Donald Trump";
+		}
+
+			if (lvl.equals("Donald Trump")) {
+				System.out.println("");
 			}
-			out.println("-----------------------------------------");
+			else{
+				System.out.println("You need $" + d.format(lvlmon - endingBal) + " to reach the next level: " + lvl);
+			}
+			System.out.println("-----------------------------------------");
 			age++;
 		}
 
